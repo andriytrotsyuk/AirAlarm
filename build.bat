d8729afb847c7348b33d9e78ffa32d5a4cf986c5
Andrii Trotsiuk <andriy.trotsyuk@gmail.com>
2025-11-03 11:11:36 +0000

    fix: windows

--- build.bat
+++ build.bat
@@ -1,8 +1,9 @@
-pyinstaller --onefile --noconsole -i win32\megaphone.ico src\airalarm.py
-xcopy src\data dist\data\ /E
-copy win32\root dist
-copy LICENSE dist
+pyinstaller --onefile --noconsole -i win32\megaphone.ico --hidden-import "PIL._tkinter_finder" src\airalarm.py || exit /b %ERRORLEVEL%
+xcopy src\_internal dist\_internal\ /E || exit /b %ERRORLEVEL%
+copy win32\root dist || exit /b %ERRORLEVEL%
+copy LICENSE dist || exit /b %ERRORLEVEL%
 set name=airalarm_windows_v%1
-rename dist "%name%"
-tar -cf "%name%.zip" "%name%"
-rmdir /S /Q build
+rename dist "%name%" || exit /b %ERRORLEVEL%
+7z a -tzip "%name%.zip" "%name%" || exit /b %ERRORLEVEL%
+rmdir /S /Q build || exit /b %ERRORLEVEL%
+rmdir /S /Q "%name%" || exit /b %ERRORLEVEL%
