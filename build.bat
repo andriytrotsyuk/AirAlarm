dea98de3e3d4310a96d5575dc10474561a4d1f52
Andrii Trotsiuk <andriy.trotsyuk@gmail.com>
2026-01-08 23:17:39 +0200

    project structure for flutter

--- build.bat
+++
@@ -1,9 +0,0 @@
-pyinstaller --onefile --noconsole -i win32\megaphone.ico --hidden-import "PIL._tkinter_finder" src\airalarm.py || exit /b %ERRORLEVEL%
-xcopy src\_internal dist\_internal\ /E || exit /b %ERRORLEVEL%
-copy win32\root dist || exit /b %ERRORLEVEL%
-copy LICENSE dist || exit /b %ERRORLEVEL%
-set name=airalarm_windows_v%1
-rename dist "%name%" || exit /b %ERRORLEVEL%
-7z a -tzip "%name%.zip" "%name%" || exit /b %ERRORLEVEL%
-rmdir /S /Q build || exit /b %ERRORLEVEL%
-rmdir /S /Q "%name%" || exit /b %ERRORLEVEL%
