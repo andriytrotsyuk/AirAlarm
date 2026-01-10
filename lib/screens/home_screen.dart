015a6a59bd5e8d8b28a200a1abbbce424b9ca657
Andrii Trotsiuk <andriy.trotsyuk@gmail.com>
2026-01-10 13:44:49 +0200

    do not close app by x

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:window_manager/window_manager.dart';
+import 'package:tray_manager/tray_manager.dart';
+import 'dart:io';
+
 import '../providers/app_state.dart';
 import 'views/status_view.dart';
 import 'views/search_view.dart';
@@ -13,13 +17,46 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    with WindowListener, TrayListener {
   String _version = '';
 
   @override
   void initState() {
     super.initState();
+    windowManager.addListener(this);
+    trayManager.addListener(this);
+    _init();
     _loadVersion();
+  }
+
+  @override
+  void dispose() {
+    windowManager.removeListener(this);
+    trayManager.removeListener(this);
+    super.dispose();
+  }
+
+  void _init() async {
+    await windowManager.setPreventClose(true);
+    await trayManager.setIcon(
+      Platform.isWindows ? 'assets/icon.ico' : 'assets/icon.png',
+    );
+    await trayManager.setToolTip('Повітряна тривога');
+    Menu menu = Menu(
+      items: [
+        MenuItem(
+          key: 'show_window',
+          label: 'Відкрити',
+        ),
+        MenuItem.separator(),
+        MenuItem(
+          key: 'exit_app',
+          label: 'Вийти',
+        ),
+      ],
+    );
+    await trayManager.setContextMenu(menu);
   }
 
   Future<void> _loadVersion() async {
@@ -27,6 +64,35 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {
       _version = packageInfo.version;
     });
+  }
+
+  @override
+  void onWindowClose() async {
+    bool isPreventClose = await windowManager.isPreventClose();
+    if (isPreventClose) {
+      windowManager.hide();
+    }
+  }
+
+  @override
+  void onTrayIconMouseDown() {
+    windowManager.show();
+    windowManager.focus();
+  }
+
+  @override
+  void onTrayIconRightMouseDown() {
+    trayManager.popUpContextMenu();
+  }
+
+  @override
+  void onTrayMenuItemClick(MenuItem menuItem) {
+    if (menuItem.key == 'show_window') {
+      windowManager.show();
+      windowManager.focus();
+    } else if (menuItem.key == 'exit_app') {
+      windowManager.destroy();
+    }
   }
 
   @override
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,6 +10,7 @@ import package_info_plus
 import path_provider_foundation
 import screen_retriever_macos
 import shared_preferences_foundation
+import tray_manager
 import window_manager
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
@@ -18,5 +19,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   ScreenRetrieverMacosPlugin.register(with: registry.registrar(forPlugin: "ScreenRetrieverMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  TrayManagerPlugin.register(with: registry.registrar(forPlugin: "TrayManagerPlugin"))
   WindowManagerPlugin.register(with: registry.registrar(forPlugin: "WindowManagerPlugin"))
 }
